5e1e39f0ffffa46a418a0c2e1d397af36b60a547
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-06-24 13:59:09 -0700

    created components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
